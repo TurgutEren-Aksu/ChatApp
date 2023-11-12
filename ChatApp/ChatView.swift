3b0e71df9e580bbe9d575bc9402866e180d0b896
Turgut Eren Aksu <turguterenaksu@outlook.com>
2023-11-13 00:03:36 +0300

    ChatView add

---
+++ ChatApp/ChatView.swift
@@ -0,0 +1,41 @@
+import Foundation
+import SwiftUI
+
+struct ChatView: View {
+	@State private var messageText: String = ""
+	@State private var messages: [String] = []
+
+	var body: some View {
+		VStack {
+			List(messages, id: \.self) { message in
+				Text(message)
+			}
+			
+			HStack {
+				TextField("Type a message: ", text: $messageText)
+					.textFieldStyle(RoundedBorderTextFieldStyle())
+					.padding(.horizontal)
+				
+				Button("Send") {
+					sendMessage()
+				}
+				.padding(.trailing)
+			}
+		}
+		.navigationTitle("Chat")
+	}
+
+	private func sendMessage() {
+		if !messageText.isEmpty {
+			messages.append(messageText)
+			messageText = ""
+		}
+	}
+}
+
+struct ChatView_Previews: PreviewProvider {
+	static var previews: some View {
+		ChatView()
+	}
+}
+
