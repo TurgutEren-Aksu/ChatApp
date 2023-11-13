44dca416d02a0bd497e24e7a3003094e378c98a8
Turgut Eren Aksu <turguterenaksu@outlook.com>
2023-11-13 23:38:34 +0300

    Update AppDelegate

--- ChatApp/AppDelegate.swift
+++ ChatApp/AppDelegate.swift
@@ -1,8 +1,10 @@
-//
-//  AppDelegate.swift
-//  ChatApp
-//
-//  Created by Turgut Eren Aksu on 13.11.2023.
-//
+import UIKit
+import Firebase
 
-import Foundation
+class AppDelegate: NSObject, UIApplicationDelegate {
+	
+	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+		FirebaseApp.configure()
+		return true
+	}
+}
