fc50da34330c7b0803900777e38494fe09f22f34
Turgut Eren Aksu <turguterenaksu@outlook.com>
2023-12-10 17:26:26 +0300

    sendButton activated

--- ChatApp/ChatView.swift
+++ ChatApp/ChatView.swift
@@ -3,56 +3,66 @@ import SwiftUI
 import FirebaseCore
 import FirebaseFirestore
 import FirebaseAuth
-
+class SendButton: ObservableObject{
+	
+	@Published var messageText = ""
+	
+	init(){
+		
+	}
+	func handleSend(){
+		print(messageText)
+	}
+}
 struct ChatView: View{
 	
 	let chatUser: ChatUser?
 	@State var messageText = ""
+	@ObservedObject var vm = SendButton()
 	
 	var body: some View{
-		ZStack{
-			messageTopBar
-			VStack{
-				Spacer()
-				chatBottomBar
-					.background(Color.white)
-			}
-			
-		}
-		
-		
+		messageTopBar
 		.navigationTitle(chatUser?.email ?? "")
 		.navigationBarTitleDisplayMode(.inline)
 	}
 	private var messageTopBar: some View {
-		ScrollView{
-			ForEach(0..<30){ num in
-				HStack{
-					Spacer ()
-					HStack{
-						Text("FAKE MESSAGE FOR NOW")
-							.foregroundStyle(.white)
+		VStack{
+			if #available(iOS 15.0, *){
+				ScrollView{
+					ForEach(0..<30){ num in
+						HStack{
+							Spacer ()
+							HStack{
+								Text("FAKE MESSAGE FOR NOW")
+									.foregroundStyle(.white)
+							}
+							.padding( )
+							.background(Color.blue)
+							.cornerRadius(8)
+							
+						}
+						.padding(.horizontal)
+						.padding(.top, 8)
+						
 					}
-					.padding( )
-					.background(Color.blue)
-					.cornerRadius(8)
+					HStack{ Spacer() }
 					
 				}
-				.padding(.horizontal)
-				.padding(.top, 8)
+				.background(Color(.init(white: 0.95, alpha: 1)))
+				.safeAreaInset(edge: .bottom) {
+					chatBottomBar
+						.background(Color(.systemBackground).ignoresSafeArea())
+				}
+			}else {
 				
 			}
-			HStack{ Spacer() }
-			
 		}
-		.background(Color(.init(white: 0.95, alpha: 1)))
-		.padding(.bottom,65)
 	}
 	private var chatBottomBar: some View {
 		HStack(spacing: 16){
 			TextField("Descripttion", text: $messageText)
 			Button {
-				
+				vm.handleSend()
 			} label: {
 				Text("Send")
 					.foregroundStyle(Color(.white))
